971acd6a5b30e39e6a4a56ee2e2d59e63f88d5d8
James Collier <jamescollier7@users.noreply.github.com>
2023-05-09 11:02:11 -0600

    show gamebaord

--- src/js/game.js
+++ src/js/game.js
@@ -2,4 +2,9 @@
   "use strict"
 
   console.log(`Wanna play a game? :)`)
+
+  const gameBoardWrapperElement = document.getElementById(`game-board-wrapper`)
+  const gameBoardElement = document.getElementById(`game-board`)
+
+  showElement(gameBoardWrapperElement)
 })()
