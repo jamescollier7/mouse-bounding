ed96ab469cc9705bd4972e24c4d81711616d9edd
James Collier <jamescollier7@users.noreply.github.com>
2023-05-21 10:27:59 -0600

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,6 @@
-;(() => {
+import { showElement, hideElement } from "./modules/utilities.mjs";
+
+(() => {
   // loadScript(`src/js/utilities.js`)
   // loadScript(`src/js/sheets.js`)
   // loadScript(`src/js/html-include.js`)
@@ -11,3 +13,6 @@
     document.head.appendChild(script)
   }
 })()
+
+// test
+hideElement(document.body)
